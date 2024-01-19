494d0730da3c95946d0600dfd0fad1e15abcd17f
Kailash Nath <kailashnathashok@gmail.com>
2024-01-19 13:03:57 +0300

    Modified the QuoteView

--- BB Quotes/QuoteView.swift
+++ BB Quotes/QuoteView.swift
@@ -9,10 +9,61 @@ import SwiftUI
 
 struct QuoteView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            ZStack {
+                Image(.breakingbad)
+                    .resizable()
+                    .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
+                
+                VStack {
+                    Spacer()
+                    
+                    Text("\"You either run from things, or you face them, Mr. White.\"")
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.black.opacity(0.5))
+                        .cornerRadius(25)
+                        .padding(.horizontal)
+                    
+                    ZStack(alignment: .bottom) {
+                        Image(.jessepinkman)
+                            .resizable()
+                            .scaledToFill()
+                        
+                        Text("Jesse Pinkman")
+                            .foregroundColor(.white)
+                            .padding(10)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/) // it makes the jesse box take the whole bottom
+                            .background(.ultraThinMaterial)
+                    }
+                    .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                    .cornerRadius(80)
+                    
+                    Spacer()
+                    
+                    Button("Get Random Quote") {
+                        
+                    }
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.breakingBadGreen)
+                    .cornerRadius(7)
+                    .shadow(color: .breakingBadYellow, radius: 2)
+                    
+                    Spacer()
+                }
+                .frame(width: geo.size.width)
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
+            // the size.width ensures that the content takes up the full screen regardless of device
+        }
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
     QuoteView()
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
 }
