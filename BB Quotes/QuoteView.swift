86c5f3a3c31fd824dcf938721944fa3c07807028
Kailash Nath <kailashnathashok@gmail.com>
2024-01-19 13:02:02 +0300

    Made a sample for the content view

---
+++ BB Quotes/QuoteView.swift
@@ -0,0 +1,18 @@
+//
+//  QuoteView.swift
+//  BB Quotes
+//
+//  Created by Kailash Nath on 19/01/2024.
+//
+
+import SwiftUI
+
+struct QuoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuoteView()
+}
